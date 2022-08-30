9f35d1e83af2bdfef4daedea9f96e0d7c2219f08
Vishall001 <vshmandloi@gmail.com>
2022-08-30 17:55:10 +0530

    some changes

--- crypto_tracker/src/Components/Coin.jsx
+++ crypto_tracker/src/Components/Coin.jsx
@@ -10,15 +10,15 @@ function Coin({ image, name, symbol, price, volume, priceChange ,marketcap}) {
           <p className="coin-symbol">{symbol}</p>
         </div>
         <div className="coin-data">
-          <p className="coin-price">${price}</p>
-          <p className="coin-volume">${volume.toLocaleString()}</p>
+          <p className="coin-price">₹{price}</p>
+          <p className="coin-volume">₹{volume.toLocaleString()}</p>
           {priceChange < 0 ? (
             <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
           ) : (
             <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
           )}
           <p className="coin-market">
-            Mkt Cap :${marketcap.toLocaleString()}
+            Mkt Cap :₹{marketcap.toLocaleString()}
           </p>
         </div>
       </div>
